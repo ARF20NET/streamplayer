60b0cfe75b440ab163b14a3b189843e0fcc01ca6
arf20 <aruizfernandez05@gmail.com>
2023-03-15 16:24:34 +0100

    Fixed the path i think

--- index.php
+++ index.php
@@ -7,10 +7,40 @@
 		$playstream = "/hls/arf20.m3u8";
 	else
 		$playstream = $_GET["stream"];
-		
+
+	function streamExists($stream) {
+		$headers = @get_headers("http://arf20.com/".$stream);
+		return $headers && strpos($headers[0], '200');
+	}
+
 	$live = false;
-	if (file_exists("/mnt".$playstream))
+	if (streamExists($playstream))
 		$live = true;
+
+	function getStreams() {
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_URL, "http://arf20.com/hls/");
+		curl_setopt($ch, CURLOPT_SSH_COMPRESSION, true);
+		curl_setopt_array($ch, [
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_URL => "https://arf20.com/hls/"
+		]);
+		$result = curl_exec($ch);
+
+		$streams = array();
+
+		$line = strtok($result, "\n");
+		while ($line !== false) {
+			$line = strtok("\n");
+			if (!strpos($line, ".m3u8")) continue;
+			$p = strpos($line, "href=\"") + 6;
+			array_push($streams, substr($line, $p, strpos($line, ".m3u8") - $p));
+			//array_push($streams, $line);
+		}
+
+		return $streams;
+	}
 ?>
 
 <!DOCTYPE html>
@@ -72,22 +102,14 @@
 		<div>
 			<h3>Streams Live Now</h3>
 			<?php
-				$files = scandir("/mnt/hls");
-				$streams = array();
-				
-				foreach ($files as $file) {
-					if (strpos($file, ".m3u8"))
-						array_push($streams, $file);
-				}
-				
-				foreach ($streams as $stream) {
-					echo '<a href="https://arf20.com/stream.php?stream=/hls/'.$stream.'">/hls/'.$stream.'</a><br>';
+				foreach (getStreams() as $stream) {
+					echo '<a href="https://arf20.com/stream.php?stream=/hls/'.$stream.'.m3u8">'.$stream.'</a><br>';
 				}
 			?>
 		</div>
 		<div>
 			<h3>Now YOU can stream too!</h3>
-			<p>Simply open OBS and set stream URL to rtmp://arf20.com/show, and you choose the stream <i>key</i> that will me the stream name.<br>
+			<p>Simply open OBS and set stream URL to rtmp://arf20.com/show, and you choose the stream <i>key</i> that will be the stream name.<br>
 			Then the stream is distributed at http://arf20.com/hls/<i>key</i>.m3u8, and vieweable at https://arf20.com/stream.php?stream=/hls/<i>key</i>.m3u8</p>
 		</div>
 	</body>
